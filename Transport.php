ef19f18abe8e4de9e2e09366bcbd4c0897eba8d4
Melnikov Ruslan <melnikov@novator.angara.ru>
2015-11-19 11:07:45 +0800

    Refactoring

--- Transport.php
+++ Transport.php
@@ -1,9 +1,13 @@
 <?php
+
 namespace mrssoft\smsintel;
 
+/**
+ * Отпарвка SMS
+ * http://smsintel.ru/
+ */
 class Transport
 {
-
     /**
      * @var int 1 - использовать HTTPS-адрес, 0 - HTTP
      */
@@ -30,14 +34,21 @@ class Transport
     public $httpAddress = 'http://lcab.smsintel.ru/';
 
     /**
-     * @var string метод, которым отправляется запрос (curl или file_get_contents)
-     */
-    public $httpsMethod = 'curl';
-
-    /**
      * @var bool разрешить отправку в DEV режиме
      */
-    public $enableDev = false;
+    public $enableDev = FALSE;
+
+    public $onlydelivery = 1;
+
+    /**
+     * @var int 1 - отправлять через дорогой канал с согласованным именем отправителя. 0 - дешёвый канал.
+     */
+    public $use_alfasource = 1;
+
+    /**
+     * @var string имя отправителя
+     */
+    public $source;
 
     /**
      * Проверка баланса
@@ -45,73 +56,43 @@ class Transport
      */
     public function balance()
     {
-        return $this->get($this->request("balance"), "account");
+        return $this->get($this->request('balance'), 'account');
     }
 
-    public function reports($start = "0000-00-00", $stop = "0000-00-00", $dop = [])
+    /**
+     * Список рассылок за определенный период
+     * @param string $start
+     * @param string $stop
+     * @param array $params
+     * @return array
+     */
+    public function reports($start = '0000-00-00', $stop = '0000-00-00', array $params = [])
     {
-        if (!isset($dop["source"]))
-        {
-            $dop["source"] = "%";
+        if (!isset($params['source'])) {
+            $params['source'] = '%';
         }
-        if (!isset($dop["number"]))
-        {
-            $dop["number"] = "%";
+        if (!isset($params['number'])) {
+            $params['number'] = '%';
         }
 
-        $result = $this->request("report", [
-            "start" => $start,
-            "stop" => $stop,
-            "source" => $dop["source"],
-            "number" => $dop["number"],
+        $result = $this->request('report', [
+            'start'  => $start,
+            'stop'   => $stop,
+            'source' => $params['source'],
+            'number' => $params['number'],
         ]);
-        if ($this->get($result, "code") != 1)
-        {
-            $return = ["code" => $this->get($result, "code"), "descr" => $this->get($result, "descr")];
-        }
-        else
-        {
+        if ($this->get($result, 'code') != 1) {
+            $return = ['code' => $this->get($result, 'code'), 'descr' => $this->get($result, 'descr')];
+        } else {
             $return = [
-                "code" => $this->get($result, "code"),
-                "descr" => $this->get($result, "descr"),
+                'code'  => $this->get($result, 'code'),
+                'descr' => $this->get($result, 'descr'),
             ];
-            if (isset($result['sms']))
-            {
-                if (!isset($result['sms'][0]))
-                {
+            if (isset($result['sms'])) {
+                if (!isset($result['sms'][0])) {
                     $result['sms'] = [$result['sms']];
                 }
-                $return["sms"] = $result['sms'];
-            }
-        }
-
-        return $return;
-    }
-
-    public function detailReport($smsid)
-    {
-        $result = $this->request("report", ["smsid" => $smsid]);
-        if ($this->get($result, "code") != 1)
-        {
-            $return = ["code" => $this->get($result, "code"), "descr" => $this->get($result, "descr")];
-        }
-        else
-        {
-            $detail = $result["detail"];
-            $return = [
-                "code" => $this->get($result, "code"),
-                "descr" => $this->get($result, "descr"),
-                "delivered" => $detail['delivered'],
-                "notDelivered" => $detail['notDelivered'],
-                "waiting" => $detail['waiting'],
-                "process" => $detail['process'],
-                "enqueued" => $detail['enqueued'],
-                "cancel" => $detail['cancel'],
-                "onModer" => $detail['onModer'],
-            ];
-            if (isset($result['sms']))
-            {
-                $return["sms"] = $result['sms'];
+                $return['sms'] = $result['sms'];
             }
         }
 
@@ -119,171 +100,171 @@ class Transport
     }
 
     /**
-     * отправка смс
-     * @param array $params (text => , source =>, datetime => , action =>, onlydelivery =>, smsid =>)
-     * @param array $phones
+     * Детализация рассылки
+     * @param mixed $smsid
      * @return array
      */
-    public function send($params = [], $phones = [])
+    public function detailReport($smsid)
     {
-        if (!$this->enableDev)
-        {
-            if (defined('YII_ENV') && YII_ENV == 'dev') return ['code' => 1];
+        $result = $this->request('report', ['smsid' => $smsid]);
+        if ($this->get($result, 'code') != 1) {
+            $return = ['code' => $this->get($result, 'code'), 'descr' => $this->get($result, 'descr')];
+        } else {
+            $detail = $result['detail'];
+            $return = [
+                'code'         => $this->get($result, 'code'),
+                'descr'        => $this->get($result, 'descr'),
+                'delivered'    => $detail['delivered'],
+                'notDelivered' => $detail['notDelivered'],
+                'waiting'      => $detail['waiting'],
+                'process'      => $detail['process'],
+                'enqueued'     => $detail['enqueued'],
+                'cancel'       => $detail['cancel'],
+                'onModer'      => $detail['onModer'],
+            ];
+            if (isset($result['sms'])) {
+                $return['sms'] = $result['sms'];
+            }
+        }
+
+        return $return;
+    }
+
+    /**
+     * Отправка смс
+     * @param array|string $params (text => , source =>, datetime => , action =>, onlydelivery =>, smsid =>)
+     * @param array|string $phones
+     * @return array
+     */
+    public function send($params, $phones)
+    {
+        if (!$this->enableDev && YII_ENV == 'dev') {
+            return ['code' => 1];
         }
 
         $phones = (array)$phones;
-        if (!isset($params["action"]))
-        {
-            $params["action"] = "send";
+        if (!isset($params['action'])) {
+            $params['action'] = 'send';
         }
-        $someXML = "";
-        if (isset($params["text"]))
-        {
-            $params["text"] = htmlspecialchars($params["text"]);
+
+        $someXML = '';
+
+        if (!is_array($params)) {
+            $params['text'] = $params;
         }
-        foreach ($phones as $phone)
-        {
-            if (is_array($phone))
-            {
-                if (isset($phone["number"]))
-                {
-                    $someXML .= "<to number='" . $phone['number'] . "'>";
-                    if (isset($phone["text"]))
-                    {
-                        $someXML .= htmlspecialchars($phone["text"]);
+
+        if (isset($params['text'])) {
+            $params['text'] = htmlspecialchars($params['text']);
+        }
+
+        if (!isset($params['onlydelivery'])) {
+            $params['onlydelivery'] = $this->onlydelivery;
+        }
+
+        if (!isset($params['use_alfasource'])) {
+            $params['use_alfasource'] = $this->use_alfasource;
+        }
+
+        foreach ($phones as $phone) {
+            if (is_array($phone)) {
+                if (isset($phone['number'])) {
+                    $someXML .= '<to number="'.$phone['number'].'">';
+                    if (isset($phone['text'])) {
+                        $someXML .= htmlspecialchars($phone['text']);
                     }
-                    $someXML .= "</to>";
+                    $someXML .= '</to>';
                 }
-            }
-            else
-            {
-                $someXML .= "<to number='$phone'></to>";
+            } else {
+                $someXML .= '<to number="'.$phone.'"></to>';
             }
         }
-        $result = $this->request("send", $params, $someXML);
-        if ($this->get($result, "code") != 1)
-        {
-            $return = ["code" => $this->get($result, "code"), "descr" => $this->get($result, "descr")];
-        }
-        else
-        {
+
+        $result = $this->request('send', $params, $someXML);
+        if ($this->get($result, 'code') != 1) {
             $return = [
-                "code" => 1,
-                "descr" => $this->get($result, "descr"),
-                "datetime" => $this->get($result, "datetime"),
-                "action" => $this->get($result, "action"),
-                "allRecivers" => $this->get($result, "allRecivers"),
-                "colSendAbonent" => $this->get($result, "colSendAbonent"),
-                "colNonSendAbonent" => $this->get($result, "colNonSendAbonent"),
-                "priceOfSending" => $this->get($result, "priceOfSending"),
-                "colsmsOfSending" => $this->get($result, "colsmsOfSending"),
-                "price" => $this->get($result, "price"),
-                "smsid" => $this->get($result, "smsid"),
+                'code'  => $this->get($result, 'code'),
+                'descr' => $this->get($result, 'descr')
+            ];
+        } else {
+            $return = [
+                'code'              => 1,
+                'descr'             => $this->get($result, 'descr'),
+                'datetime'          => $this->get($result, 'datetime'),
+                'action'            => $this->get($result, 'action'),
+                'allRecivers'       => $this->get($result, 'allRecivers'),
+                'colSendAbonent'    => $this->get($result, 'colSendAbonent'),
+                'colNonSendAbonent' => $this->get($result, 'colNonSendAbonent'),
+                'priceOfSending'    => $this->get($result, 'priceOfSending'),
+                'colsmsOfSending'   => $this->get($result, 'colsmsOfSending'),
+                'price'             => $this->get($result, 'price'),
+                'smsid'             => $this->get($result, 'smsid'),
             ];
         }
 
         return $return;
-
     }
 
     private function get($responce, $key)
     {
-        if (isset($responce[$key]))
-        {
+        if (isset($responce[$key])) {
             return $responce[$key];
         }
 
-        return false;
+        return FALSE;
     }
 
     private function getURL($action)
     {
-        if ($this->useHttps == 1)
-        {
-            $address = $this->httpsAddress . "API/XML/" . $action . ".php";
+        if ($this->useHttps == 1) {
+            $address = $this->httpsAddress.'API/XML/'.$action.'.php';
+        } else {
+            $address = $this->httpAddress.'API/XML/'.$action.'.php';
         }
-        else
-        {
-            $address = $this->httpAddress . "API/XML/" . $action . ".php";
-        }
-        $address .= "?returnType=json";
+        $address .= '?returnType=json';
 
         return $address;
     }
 
-    private function request($action, $params = [], $someXML = "")
+    private function request($action, array $params = [], $someXML = '')
     {
         $xml = $this->makeXML($params, $someXML);
-        if ($this->httpsMethod == "curl")
-        {
-            $res = $this->request_curl($action, $xml);
-        }
-        elseif ($this->httpsMethod == "file_get_contents")
-        {
-            $opts = [
-                'http' => [
-                    'method' => 'POST',
-                    'header' => 'Content-type: application/x-www-form-urlencoded',
-                    'content' => $xml
-                ]
-            ];
 
-            $context = stream_context_create($opts);
-
-            $res = file_get_contents($this->getURL($action), false, $context);
-        }
-        if (isset($res))
-        {
-            $res = json_decode($res, true);
-            if (isset($res["data"]))
-            {
-                return $res["data"];
-            }
-
-            return [];
-        }
-        $this->error("В настройках указан неизвестный метод запроса - '" . $this->httpsMethod . "'");
-
-        return null;
-    }
-
-    private function request_curl($action, $xml)
-    {
-        $address = $this->getURL($action);
-        $ch = curl_init($address);
-        curl_setopt($ch, CURLOPT_URL, $address);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->getURL($action));
         curl_setopt($ch, CURLOPT_FAILONERROR, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         $result = curl_exec($ch);
         curl_close($ch);
 
-        return $result;
+        if (isset($result)) {
+            $result = json_decode($result, TRUE);
+            if (isset($result['data'])) {
+                return $result['data'];
+            }
+
+            return [];
+        }
+
+        return NULL;
     }
 
-    private function makeXML($params, $someXML = "")
+    private function makeXML($params, $someXML = '')
     {
-        $xml = "<?xml version='1.0' encoding='UTF-8'?>
-		<data>
-			<login>" . htmlspecialchars($this->httpsLogin) . "</login>
-			<password>" . htmlspecialchars($this->httpsPassword) . "</password>
-			";
-        foreach ($params as $key => $value)
-        {
+        $xml = '';
+        foreach ($params as $key => $value) {
             $xml .= "<$key>$value</$key>";
         }
-        $xml .= "$someXML
-		</data>";
+
+        $xml = '<?xml version="1.0" encoding="UTF-8"?><data><login>'
+            .htmlspecialchars($this->httpsLogin).'</login><password>'
+            .htmlspecialchars($this->httpsPassword).'</password>'
+            .$xml.$someXML.'</data>';
 
         return $xml;
-    }
-
-    private function error($text)
-    {
-        die($text);
     }
 }
